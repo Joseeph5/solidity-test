6b8d3800bfaeacd9cd03a9f9977681ef65ff6da1
Joseeph5 <youssef.beenamor@gmail.com>
2022-08-22 11:24:47 +0100

    write some tests

--- test/Certification.js
+++ test/Certification.js
@@ -1,5 +1,4 @@
-const { time, loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { anyValue } = require('@nomicfoundation/hardhat-chai-matchers/withArgs');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
@@ -12,17 +11,29 @@ describe('Certification Deployment', async function () {
 
     Certification = await ethers.getContractFactory('Certification');
     certi = await Certification.deploy();
-
     return { certi, owner, otherAccount };
   }
 
   it('Deployment successful', async function () {
-    const { certi, owner } = await loadFixture(deployOneYearLockFixture);
+    const { certi } = await loadFixture(deployOneYearLockFixture);
     expect(certi).to.not.be.null;
   });
 
   it('Should set the right owner', async function () {
     const { certi, owner } = await loadFixture(deployOneYearLockFixture);
     expect(await certi.owner()).to.equal(owner.address);
+  });
+
+  it('Should validate a student if called from the owner', async function () {
+    const { certi, otherAccount } = await loadFixture(deployOneYearLockFixture);
+    const validate = await certi.validateStudent([otherAccount.address]);
+    expect(validate).to.have.property('hash');
+  });
+
+  it('Should revert with the right error if called from another account', async function () {
+    const { certi, otherAccount } = await loadFixture(deployOneYearLockFixture);
+    await expect(
+      certi.connect(otherAccount).validateStudent([otherAccount.address])
+    ).to.be.revertedWith('Ownable: caller is not the owner');
   });
 });
